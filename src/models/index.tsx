630103aad899abf8313131bb0a276b98cf0902dd
Tom Wright <tom.w.wright@live.com>
2018-01-27 10:34:08 +1100

    Types for game

--- src/models/index.tsx
+++ src/models/index.tsx
@@ -3,6 +3,38 @@ export type ObjectWithId = {
 };
 
 export type Game = ObjectWithId & {
-  turn: number;
   players: string[];
+  currentRound: number;
+  devices: Device[];
+  rounds: Round[];
+};
+
+export type Device = {
+  name: string;
+  colour: string;
+  type: string;
+  playerName: string;
+};
+
+export type Instruction = {
+  player: string;
+  device: string;
+  targetState: string;
+  targetTurn: number;
+};
+
+export type Round = {
+  currentTurn: number;
+  numTurns: number;
+  turnDuration: number;
+  difficulty: number;
+  lives: number;
+  usedLives: number;
+  turns: Turn[];
+};
+
+export type Turn = {
+  targetState: { [device: string]: string };
+  deviceState: { [device: string]: string };
+  instructions: Instruction[];
 };
