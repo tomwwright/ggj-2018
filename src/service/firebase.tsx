7780356fc0c034c6092679bfe4e322045c22a7ed
sampsonoliver <sampsonjoliver@outlook.com>
2018-01-26 22:30:44 +1100

    Added firebase stuff and prettier

---
+++ src/service/firebase.tsx
@@ -0,0 +1,27 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+import { ObjectWithId } from "models";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+
+function mapDocToT<T extends ObjectWithId>(doc: firebase.firestore.DocumentSnapshot): T {
+  return {
+    id: doc.id,
+    ...doc.data()
+  } as T;
+}
+
+export default firebase;
+export { firestore, auth, mapDocToT };
