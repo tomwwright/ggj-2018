13c14afc593b4c9456ce944ddf2bc75403b6d042
Tom Wright <tom.w.wright@live.com>
2018-01-26 21:58:31 +1100

    Initial project

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "typeface-roboto";
+import { Provider } from "mobx-react";
+
+ReactDOM.render(<p>GGJ 2018, bruh.</p>, document.getElementById("react-container"));
