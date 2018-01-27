1beed7eb21a7c9cf8126a6d699eb90368caec604
Unknown <jthegedus@jttech.io>
2018-01-27 22:31:16 +1100

    wip backgrounds for lobby

--- src/containers/JoinGame.tsx
+++ src/containers/JoinGame.tsx
@@ -20,6 +20,22 @@ class JoinGameComponent extends React.Component<JoinGameProps, JoinGameState> {
     readied: false
   };
 
+  joinStyle = {
+    height: "100%",
+    width: "100%",
+    backgroundSize: "cover",
+    backgroundImage: "url(/images/ph_lobby_placeholder.png)",
+    textAlign: "center"
+  };
+
+  roomCodeStyle = {
+    marginTop: "115%"
+  };
+
+  playerNameStyle = {
+    marginTop: "28%"
+  };
+
   joinGame() {
     GameStore.joinGameAsPlayer(this.state.roomCode, this.state.playerName);
     this.setState({ readied: true });
@@ -32,24 +48,28 @@ class JoinGameComponent extends React.Component<JoinGameProps, JoinGameState> {
 
     return (
       <React.Fragment>
-        <h2>Join</h2>
-        <p>Room Code</p>
-        <input
-          type="text"
-          value={this.state.roomCode}
-          onChange={event => this.setState({ roomCode: event.target.value })}
-        />
-        <p>Hoo-hoo are you?</p>
-        <input
-          type="text"
-          value={this.state.playerName}
-          onChange={event => this.setState({ playerName: event.target.value })}
-        />
-        <button onClick={() => this.joinGame()}>Join</button>
+        <div id="join-container" style={this.joinStyle}>
+          <div>
+            <input
+              style={this.roomCodeStyle}
+              type="text"
+              value={this.state.roomCode}
+              onChange={event => this.setState({ roomCode: event.target.value })}
+            />
+          </div>
+          <div>
+            <input
+              style={this.playerNameStyle}
+              type="text"
+              value={this.state.playerName}
+              onChange={event => this.setState({ playerName: event.target.value })}
+            />
+          </div>
+          <button onClick={() => this.joinGame()}>Join</button>
+        </div>
       </React.Fragment>
     );
   }
 }
 
-const JoinGame = JoinGameComponent;
-export { JoinGame };
+export { JoinGameComponent as JoinGame };
--- src/containers/splash.tsx
+++ src/containers/splash.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 
+const splashStyle = {
+  height: "100%",
+  width: "100%",
+  backgroundSize: "cover",
+  backgroundImage: "url(/images/ph_splash_placeholder)"
+};
+
 export const Splash: React.StatelessComponent = ({ children }) => (
   <React.Fragment>
-    <p>You splashin', bruh.</p>
-    {children}
+    <div id="splash-container" style={splashStyle}>
+      {children}
+    </div>
   </React.Fragment>
 );
