ba803d955cdc6312eb59a7848843f997b997ba05
sampsonoliver <sampsonjoliver@outlook.com>
2018-01-27 13:15:03 +1100

    Add JoinGame readyUp screen (Y)

--- src/containers/playApp.tsx
+++ src/containers/playApp.tsx
@@ -2,24 +2,34 @@ import * as React from "react";
 import { inject, observer } from "mobx-react";
 
 import { GameStore } from "stores/game";
+import { JoinGame } from "containers/JoinGame";
 
 type PlayAppProps = {
   gameStore?: GameStore;
 };
 
-const PlayAppComponent: React.StatelessComponent<PlayAppProps> = ({ gameStore }) =>
-  !gameStore.currentTurn ? (
-    <div>
-      <p>Loading Round and Turn...</p>
-    </div>
-  ) : (
-    <div>
-      <p>Play App (PLAYER_NAME, {gameStore.token})</p>
-      <p>Round: {gameStore.game.currentRound}</p>
-      <p>
-        Turn: {gameStore.round.currentTurn.toString()} / {gameStore.round.numTurns}
-      </p>
-    </div>
-  );
+const PlayAppComponent: React.StatelessComponent<PlayAppProps> = ({ gameStore }) => {
+  if (!gameStore.playerName) {
+    return <JoinGame />;
+  }
+
+  if (!gameStore.currentTurn) {
+    return (
+      <React.Fragment>
+        <p>Loading Round and Turn...</p>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <p>Play App (PLAYER_NAME, {gameStore.token})</p>
+        <p>Round: {gameStore.game.currentRound}</p>
+        <p>
+          Turn: {gameStore.round.currentTurn.toString()} / {gameStore.round.numTurns}
+        </p>
+      </React.Fragment>
+    );
+  }
+};
 
 export const PlayApp = inject("gameStore")(observer(PlayAppComponent));
