887cfbaa88a0bf7b37d0fe47e40cd5ce44463032
Unknown <jthegedus@jttech.io>
2018-01-27 12:47:39 +1100

    setup root page routing to createGame page etc

--- src/containers/root.tsx
+++ src/containers/root.tsx
@@ -1,31 +1,58 @@
 import * as React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Link } from "react-router-dom";
+import { Redirect } from "react-router";
 
 import { PlayApp } from "./playApp";
 import { TvApp } from "./tvApp";
 import { WithGame } from "containers/withGame";
+import { CreateGame } from "containers/CreateGame";
+
+const tokenExists = (token): boolean => {
+  // TODO: check if token exists
+  return true;
+};
 
 export const Root: React.StatelessComponent = () => (
   <BrowserRouter>
     <Switch>
       <Route
+        exact
+        path="/"
+        render={props => {
+          return <CreateGame />;
+        }}
+      />
+      <Route
         path="/play/:token"
-        render={props => (
-          <WithGame token={props.match.params.token}>
-            <PlayApp />
-          </WithGame>
-        )}
+        render={props => {
+          return tokenExists(props.match.params.token) ? (
+            <WithGame token={props.match.params.token}>
+              <PlayApp />
+            </WithGame>
+          ) : (
+            // route to createGame page
+            <Redirect to="/" />
+          );
+        }}
       />
       <Route
         path="/tv/:token"
-        render={props => (
-          <WithGame token={props.match.params.token}>
-            <TvApp />
-          </WithGame>
-        )}
+        render={props => {
+          return tokenExists(props.match.params.token) ? (
+            <WithGame token={props.match.params.token}>
+              <TvApp />
+            </WithGame>
+          ) : (
+            // route to createGame page
+            <Redirect to="/" />
+          );
+        }}
       />
       <Route>
-        <p>Oi</p>
+        <React.Fragment>
+          <p>BAD URL. Try:</p>
+          <Link to="/">Home</Link>
+        </React.Fragment>
       </Route>
     </Switch>
   </BrowserRouter>
