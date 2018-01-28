e71f6f21a6054088ae6244df422df6e6172e8ef5
Unknown <jthegedus@jttech.io>
2018-01-28 12:45:17 +1100

    lets do dis

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "typeface-roboto";
+import "./index.css";
 import { Provider } from "mobx-react";
 
 import { Root } from "./containers/root";
